1057069767d0482d9b36ba273bded74f1e8014ff
mya0 <30719221+mya0@users.noreply.github.com>
2017-09-09 21:39:44 +0900

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,7 @@
+#! /bin/sh
+
+echo "Download Unity"
+curl -o Unity.pkg https://netstorage.unity3d.com/unity/5d30cf096e79/MacEditorInstaller/Unity-2017.1.1f1.pkg
+
+echo "Install Unity"
+sudo installer -dumplog -package Unity.pkg -target /
